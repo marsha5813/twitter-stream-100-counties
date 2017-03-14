05153ef3acb799878e6370f4e36dceb97b1c2d25
Joey Marshall <marsha58@purdue.edu>
2017-03-13 23:45:57 -0400

    Makes updates to the comments in main.r

--- main.R
+++ main.R
@@ -12,25 +12,25 @@ boxes.df = read.csv("coords.csv", header=FALSE, sep=",", stringsAsFactors = FALS
 locations = as.numeric(as.matrix(boxes.df["V1"]))
 
 # Splits the location data into four lists
-# For some reason, filterStream won't allow me to input all 100 locations at once
+# The Twitter stream API allows 25 bounding boxes per streaming request
 locations1 = locations[1:100]
 locations2 = locations[101:200]
 locations3 = locations[201:300]
 locations4 = locations[301:400]
 
-# stream tweets from the first 100 locations on the list
+# stream tweets from the first 25 locations on the list
 filterStream("tweets.json", locations = locations1, tweets=1000, timeout = 300, 
              oauth = my_oauth, verbose = TRUE)
 
-# stream tweets from the second 100 locations on the list
+# stream tweets from the second 25 locations on the list
 filterStream("tweets.json", locations = locations2, tweets=1000, timeout = 300, 
              oauth = my_oauth, verbose = TRUE)
 
-# stream tweets from the third 100 locations on the list
+# stream tweets from the third 25 locations on the list
 filterStream("tweets.json", locations = locations3, tweets=1000, timeout = 300, 
              oauth = my_oauth, verbose = TRUE)
 
-# stream tweets from the fourth 100 locations on the list
+# stream tweets from the fourth 25 locations on the list
 filterStream("tweets.json", locations = locations4, tweets=1000, timeout = 300, 
              oauth = my_oauth, verbose = TRUE)
 
